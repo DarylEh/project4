e1cc27021f478dd105cd497be58888d37022fc97
Daryl Amodan <darylamodan@gmail.com>
2018-01-15 10:49:32 -0500

    cleaned code, removed console log

---
+++ public/scripts/main.js
@@ -0,0 +1,82 @@
+//the user will be able to enter characters
+//will we get data from api
+//display some stuff
+
+const app = {};
+app.baseUrl = 'http://gateway.marvel.com/v1/public/characters';
+app.publicKey = 'eea7b047875ac73ea9c0e0bbdcf9b88e';
+app.hash = '541d33f09ab47d8a76e91be1dc4d83bd';
+
+app.getData = data => data[0].data.results[0];
+app.getStoriesData = data => data[0].data.results[0].id;
+
+
+app.events = () => {
+    $('form').on('submit', (e) => {
+        e.preventDefault();
+        
+        //animation starts here
+        let animateMain = $('main');
+        let animateResults = $('.results');
+        animateMain.addClass('magictime vanishOut');
+        animateMain.one('animationend', () => {
+            animateMain.addClass('vanishIntro');
+            animateResults.addClass('magictime vanishIn');
+            animateResults.addClass('visibilityOn');
+        });
+
+        //formatting the name of what was inputted 
+        let characters = $('.characters').val();
+        characters = characters.split(',')
+            .map(name => name.trim())
+            .map(app.searchPerson);
+        //matching data with api
+        $.when(...characters)
+            .then((...characterData) => {
+                if (typeof characterData[1] === 'string') {
+                    characterData = [characterData]
+                }
+                characterData = characterData.map(app.getData);
+                app.showInfo(characterData);
+            })
+    })
+};
+
+//displaying data using template literals because it's always good to try something new.
+app.showInfo = (characterData) => {
+    $('#display').empty();
+    const template = `
+    <div class ="credit">
+    <div class = "description">
+    <h2> ${characterData[0].name}</h2>
+    <p>${characterData[0].description}</p>
+    </div>
+    <div class = "photo"> 
+    <img src="${characterData[0].thumbnail.path}/portrait_uncanny.jpg">
+    </div>
+    </div>
+    <input class="reset-btn" type="reset" value="Reset" onClick="window.location.reload()">
+    `;
+    $('#display').append(template);
+};
+
+//api request
+app.searchPerson = (name) => {
+    return $.ajax({
+        url: `${app.baseUrl}?`,
+        method: 'GET',
+        dataType: 'json',
+        data: {
+            name: name,
+            ts: '12345',
+            apikey: app.publicKey,
+            hash: app.hash
+        },
+    });
+}
+//time to run events
+app.init = () => {
+    app.events();
+}
+//initialize the app
+$(app.init);
